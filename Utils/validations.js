70d5113b0a285f6b5a0aa1cb10f815a7e29d9343
ssabhijith2000 <abhijith.ss@inapp.com>
2022-08-09 15:50:24 +0530

    completed login structure

--- Utils/validations.js
+++ Utils/validations.js
@@ -3,7 +3,8 @@ export function findIfEmpty(input) {
   }
 
 export function isValidEmail(input) {
-    if (input.trim().match('/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/'))
+    if (input.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/))
     return true;
+    else return false;
 }
 
